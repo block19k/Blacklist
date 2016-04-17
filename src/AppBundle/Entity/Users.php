51d5677e4bdb2133b3d29cb7319e4068221898f7
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2016-04-17 23:23:24 +0300

    created new database

--- src/AppBundle/Entity/Users.php
+++ src/AppBundle/Entity/Users.php
@@ -16,40 +16,9 @@ class Users extends BaseUser
     /**
      * @var string
      *
-     * @ORM\Column(name="username", type="string", length=255, nullable=true)
-     */
-    private $username;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=255, nullable=true)
-     */
-    private $password;
-
-    /**
-     * @var string
-     *
      * @ORM\Column(name="role", type="string", length=255, nullable=false)
      */
     private $role;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=255, nullable=true)
-     */
-    private $email;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
-
 
 
     /**
